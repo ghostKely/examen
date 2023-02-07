b024edc071ea1ebcf066430b77a6bb34cb91eb1c
ghostKely <120150720+ghostKely@users.noreply.github.com>
2023-02-07 13:52:57 +0300

    donne de test

--- codeiniter0_0/data.sql
+++ codeiniter0_0/data.sql
@@ -33,3 +33,29 @@ create table photo(
     path varchar(100)
 );
 
+insert into user values(null,"Balita","Tsiory","balitatsiory@gmail.com","bonjour",1);
+insert into user values(null,"Ony","Tsiory","tsiory@gmail.com","veloma",0);
+
+insert into categorie values(null,'vetement');
+insert into categorie values(null,'technologie');
+insert into categorie values(null,'bijou');
+insert into categorie values(null,'music');
+insert into categorie values(null,'utilitaire');
+insert into categorie values(null,'outil');
+
+create table objet(null,'bague');
+create table objet(null,'pc');
+create table objet(null,'ipods');
+create table objet(null,'telephone');
+create table objet(null,'chemise');
+create table objet(null,'tourne vis');
+create table objet(null,'cahier');
+create table objet(null,'tronsoneuse');
+create table objet(null,'ecouteur');
+create table objet(null,'pantalon');
+create table objet(null,'asus');
+create table objet(null,'stylo');
+create table objet(null,'rein');
+
+
+
